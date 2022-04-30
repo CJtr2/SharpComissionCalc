47dd991d6da6f461ad67b0e91e0846c72c11b9c3
Colton Jeter <coltonjeter@gmail.com>
2022-04-30 05:07:42 -0700

    -Some more initial layout work

--- app/src/main/java/com/example/comissioncalc/MainActivity.java
+++ app/src/main/java/com/example/comissioncalc/MainActivity.java
@@ -10,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //notes:
+        /*
+        on image view pressed change between toggled on text and toggled off text, make custom image for it
+        check all others to make sure they aren't enabled on press
+        have array of booleans
+
+
+        replace full body/half body/headshot with custom text
+        */
     }
 }
