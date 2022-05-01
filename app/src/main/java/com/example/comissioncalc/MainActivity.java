65c70a128d32af240f78b10bb7286da63724aa83
Colton Jeter <coltonjeter@gmail.com>
2022-04-30 18:44:37 -0700

    -Primary Backend Math Completion

--- app/src/main/java/com/example/comissioncalc/MainActivity.java
+++ app/src/main/java/com/example/comissioncalc/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.comissioncalc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
@@ -10,6 +13,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Locale;
 
@@ -51,8 +55,6 @@ public class MainActivity extends AppCompatActivity {
 
     public double preExtraFinal;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -77,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
         addExtra = findViewById(R.id.addExtraBtn);
         calculate = findViewById(R.id.calcBtn);
 
-
+        preExtraFinal = 0;
         //internal declarations (will be fetched from config)
         basePrice = 60.0;
         flatPrice = 30.0;
@@ -131,6 +133,15 @@ public class MainActivity extends AppCompatActivity {
         hbEntry.setOnFocusChangeListener(clearTextFocus);
         hsEntry.setOnFocusChangeListener(clearTextFocus);
 
+
+        copy. setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                copyText();
+            }
+        });
+
+
         //notes:
         /*
         on image view pressed change between toggled on text and toggled off text, make custom image for it
@@ -144,24 +155,12 @@ public class MainActivity extends AppCompatActivity {
         each togglable box is a different base price
         (base price * (body type modifier)) + flat [apply discounts after this]
 
-
-
-        # discounts:
-        #full + (#half)*(2/3) + (#head)/2
-        or to make decimal math less dangerous:
-
-        every full
-        every 2nd and 3rd half body
-        every other head
-
-        + floor((#half%3)/3 + (#head%2)/2)
-
-        apply to lowest first
-
         */
     }
 
     public void updatePriceText(){
+        preExtraFinal = 0;
+
         String out = "";
 
         //Data Fetching===========================================
@@ -178,21 +177,8 @@ public class MainActivity extends AppCompatActivity {
         else{hs = Integer.parseInt(temp);}
 
 
-        //# of Discount Calculation================================
-        int numOfDiscounts = fb;
-
-        for(int i = 1; i <= hb; i++){
-            if(i%hbDenom < hbNumer){
-                numOfDiscounts++;
-            }
-        }
-        for(int j = 1; j <= hs; j++){
-            if(j%hsDenom < hsNumer){
-                numOfDiscounts++;
-            }
-        }
-
-        numOfDiscounts += + Math.floor(((hb*hbNumer)%hbDenom)/((double)hbDenom) + ((hs*hsNumer)%hsDenom)/(double)hsDenom);
+        //# of Discount Calculation/Distribution=====================
+        int numOfDiscounts = ((fb+hb+hs)*discountPer1)/(discountPer1+1);
 
         int tempDis = numOfDiscounts;
         int[] discounts = new int[] {0, 0, 0};
@@ -214,23 +200,141 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+        int usedDisc = numOfDiscounts;
 
         //full body section====================================
-        if(false){
+        if(fb != 0){
+            out += "================================\n";
+            out += "Full Body:\n";
+            out += "================================\n";
+            double cost = (basePrice + flatPrice);
+            double discAmnt = cost - (cost * (discountpercent/100.0));
 
+            double dscTot = 0;
+            double costTot = 0;
+
+            int numDigits = 1;
+            if(fb > 99){
+                numDigits = 3;
+            }else if(fb > 9){
+                numDigits = 2;
+            }
+
+            String format = "%0" + numDigits + "d";
+
+            for(int q = 0; q < fb; q++){
+                out += "Full Body " + String.format(Locale.ROOT,format, q+1) + ":    $ " + String.format(Locale.ROOT,"%,.2f", cost) + "\n";
+                costTot += cost;
+
+                if(discounts[0] > 0){
+                    out += "                                      -$ " + String.format(Locale.ROOT,"%,.2f", discAmnt) + "  dsc[" + usedDisc +"]\n";
+                    usedDisc--;
+                    dscTot += discAmnt;
+                    discounts[0]--;
+                }
+            }
+
+            preExtraFinal += costTot;
+            preExtraFinal -= dscTot;
+            out += "\n================================\n";
+            out +="Cost:                $ " + String.format(Locale.ROOT,"%,.2f", costTot) + "\n";
+            out +="                                      -$ " + String.format(Locale.ROOT,"%,.2f", dscTot) + "\n";
+            out +="SubTotal:        $ " + String.format(Locale.ROOT,"%,.2f", preExtraFinal) + "\n\n";
         }
 
         //half body section====================================
-        if(false){
+        if(hb != 0){
+            out += "================================\n";
+            out += "Half Body:\n";
+            out += "================================\n";
 
+            double cost = (((basePrice * hbNumer)/hbDenom)+ flatPrice);
+            double discAmnt = cost - (cost * (discountpercent/100.0));
+
+            double dscTot = 0;
+            double costTot = 0;
+
+            int numDigits = 1;
+            if(hb > 99){
+                numDigits = 3;
+            }else if(hb > 9){
+                numDigits = 2;
+            }
+
+            String format = "%0" + numDigits + "d";
+
+            for(int q = 0; q < hb; q++){
+                out += "Half Body " + String.format(Locale.ROOT,format, q+1) + ":    $ " + String.format(Locale.ROOT,"%,.2f", cost) + "\n";
+                costTot += cost;
+
+                if(discounts[1] > 0){
+                    out += "                                      -$ " + String.format(Locale.ROOT,"%,.2f", discAmnt) + "  dsc[" + usedDisc +"]\n";
+                    usedDisc--;
+                    dscTot += discAmnt;
+                    discounts[1]--;
+                }
+            }
+
+            preExtraFinal += costTot;
+            preExtraFinal -= dscTot;
+            out += "\n================================\n";
+            out +="Cost:                $ " + String.format(Locale.ROOT,"%,.2f", costTot) + "\n";
+            out +="                                      -$ " + String.format(Locale.ROOT,"%,.2f", dscTot) + "\n";
+            out +="SubTotal:        $ " + String.format(Locale.ROOT,"%,.2f", preExtraFinal) + "\n\n";
         }
 
         //headshot section=====================================
-        if(false){
+        if(hs != 0){
+            out += "================================\n";
+            out += "Headshot:\n";
+            out += "================================\n";
 
+            double cost = (((basePrice * hsNumer)/hsDenom)+ flatPrice);
+            double discAmnt = cost - (cost * (discountpercent/100.0));
+
+            double dscTot = 0;
+            double costTot = 0;
+
+            int numDigits = 1;
+            if(hs > 99){
+                numDigits = 3;
+            }else if(hs > 9){
+                numDigits = 2;
+            }
+
+            String format = "%0" + numDigits + "d";
+
+            for(int q = 0; q < hs; q++){
+                out += "Headshot " + String.format(Locale.ROOT,format, q+1) + ":    $ " + String.format(Locale.ROOT,"%,.2f", cost) + "\n";
+                costTot += cost;
+
+                if(discounts[2] > 0){
+                    out += "                                      -$ " + String.format(Locale.ROOT,"%,.2f", discAmnt) + "  dsc[" + usedDisc +"]\n";
+                    usedDisc--;
+                    dscTot += discAmnt;
+                    discounts[2]--;
+                }
+            }
+
+            preExtraFinal += costTot;
+            preExtraFinal -= dscTot;
+            out += "\n================================\n";
+            out +="Cost:                $ " + String.format(Locale.ROOT,"%,.2f", costTot) + "\n";
+            out +="                                      -$ " + String.format(Locale.ROOT,"%,.2f", dscTot) + "\n";
+            out +="SubTotal:        $ " + String.format(Locale.ROOT,"%,.2f", preExtraFinal) + "\n\n";
         }
+        if(fb+hb+hs != 0){out += "================================\n";}
 
 
         pricesFieldText.setText(out);
+    }
+
+    public void copyText(){
+        ClipboardManager cb = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        String temp = pricesFieldText.getText().toString();
+        ClipData out = ClipData.newPlainText("output", temp);
+        cb.setPrimaryClip(out);
+
+        Toast.makeText(MainActivity.this, "Copied!", Toast.LENGTH_SHORT).show();
     }
 }
