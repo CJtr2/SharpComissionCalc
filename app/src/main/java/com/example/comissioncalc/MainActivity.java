4ae265ff2ad1bc2162f1c5e35a0712bd246387f9
Colton Jeter <coltonjeter@gmail.com>
2022-05-03 04:06:08 -0700

    -Extra Add button implementation -Back Button function added -Some input cleanup -Expanded copy function -Calculate button function

--- app/src/main/java/com/example/comissioncalc/MainActivity.java
+++ app/src/main/java/com/example/comissioncalc/MainActivity.java
@@ -7,18 +7,25 @@ import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
-import android.media.Image;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.KeyEvent;
+import android.util.TypedValue;
+import android.view.ContextThemeWrapper;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.LinkedList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -34,7 +41,8 @@ public class MainActivity extends AppCompatActivity {
     public EditText hsEntry;
 
     public TextView pricesFieldText;
-    public TextView finalpriceText;
+    public TextView finalPriceText;
+    public TextView finalDiscText;
 
     public Button config;
     public Button copy;
@@ -50,6 +58,11 @@ public class MainActivity extends AppCompatActivity {
     public ImageView linesBtn;
     public ImageView sketchBtn;
     public ImageView tgmStickBtn;
+
+    public LinearLayout extrasList;
+
+    public EditText nameInput;
+    public EditText extraPriceInput;
 
     //internal declarations===============================
     public int fb;
@@ -75,11 +88,17 @@ public class MainActivity extends AppCompatActivity {
     public int hsDenom;
 
     public double preExtraFinal;
+    public double totalDiscount;
 
     public ImageView[] shadeArray;
     public double[] prices;
     public boolean[] shadeActive;
     public String[] shadeNames;
+
+    public LinkedList<String> extras;
+    public LinkedList<Double> extrasPrices;
+
+    public RelativeLayout extrasInputCtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,10 +107,19 @@ public class MainActivity extends AppCompatActivity {
 
         //initialize declarations
         preExtraFinal = 0;
+        totalDiscount = 0;
+
+        fb = 0;
+        hb = 0;
+        hs = 0;
+
         shadeArray = new ImageView[6];
         prices = new double[6];
         shadeActive = new boolean[] {true, false, false, false, false, false};
         shadeNames = new String[] {"Shaded","Cel","Flats","Lines","Sketch","Tgm"};
+
+        extras = new LinkedList<>();
+        extrasPrices = new LinkedList<>();
 
         //activity elements declaration\
         imm = (InputMethodManager) this.getSystemService(Activity.INPUT_METHOD_SERVICE);
@@ -104,7 +132,8 @@ public class MainActivity extends AppCompatActivity {
         hsEntry = findViewById(R.id.headshotNum);
 
         pricesFieldText = findViewById(R.id.pricesText);
-        finalpriceText = findViewById(R.id.finalPriceText);
+        finalPriceText = findViewById(R.id.finalPriceText);
+        finalDiscText = findViewById(R.id.finalDiscTot);
 
         config = findViewById(R.id.configBtn);
         copy = findViewById(R.id.copyBtn);
@@ -115,11 +144,17 @@ public class MainActivity extends AppCompatActivity {
         shdCtn = findViewById(R.id.shadingContainer);
 
         shadeArray[0] = shadedBtn = findViewById(R.id.shadedBtn);
-        shadeArray[1] =celBtn = findViewById(R.id.celBtn);
-        shadeArray[2] =flatsBtn = findViewById(R.id.flatsBtn);
-        shadeArray[3] =linesBtn = findViewById(R.id.linesBtn);
-        shadeArray[4] =sketchBtn = findViewById(R.id.sketchBtn);
-        shadeArray[5] =tgmStickBtn = findViewById(R.id.tgmStickBtn);
+        shadeArray[1] = celBtn = findViewById(R.id.celBtn);
+        shadeArray[2] = flatsBtn = findViewById(R.id.flatsBtn);
+        shadeArray[3] = linesBtn = findViewById(R.id.linesBtn);
+        shadeArray[4] = sketchBtn = findViewById(R.id.sketchBtn);
+        shadeArray[5] = tgmStickBtn = findViewById(R.id.tgmStickBtn);
+
+        extrasList = findViewById(R.id.extrasList);
+
+        nameInput = findViewById(R.id.nameInputText);
+        extraPriceInput = findViewById(R.id.extraPriceInputText);
+        extrasInputCtn = findViewById(R.id.extraInputContainer);
 
         shadedBtn.setImageDrawable(ResourcesCompat.getDrawable(this.getResources(), android.R.drawable.btn_star_big_on, null));
 
@@ -148,11 +183,10 @@ public class MainActivity extends AppCompatActivity {
         baseText.setText("$ " + String.format(Locale.ROOT,"%,.0f", basePrice));
         flatText.setText("$ " + String.format(Locale.ROOT,"%,.0f", flatPrice));
 
-
-        View.OnKeyListener updatePrice = (view, keyCode, keyEvent) -> {
-            if((keyEvent.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)){
+        TextView.OnEditorActionListener updatePrice = (textView, actionId, keyEvent) -> {
+            if(actionId == EditorInfo.IME_ACTION_DONE){
                 updatePriceText();
-                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                imm.hideSoftInputFromWindow(textView.getWindowToken(), 0);
                 return true;
             }
             return false;
@@ -172,9 +206,9 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        fbEntry.setOnKeyListener(updatePrice);
-        hbEntry.setOnKeyListener(updatePrice);
-        hsEntry.setOnKeyListener(updatePrice);
+        fbEntry.setOnEditorActionListener(updatePrice);
+        hbEntry.setOnEditorActionListener(updatePrice);
+        hsEntry.setOnEditorActionListener(updatePrice);
 
         fbEntry.setOnClickListener(clearText);
         hbEntry.setOnClickListener(clearText);
@@ -207,6 +241,51 @@ public class MainActivity extends AppCompatActivity {
         tgmStickBtn.setOnClickListener(view -> toggleShade(5));
 
 
+        addExtra.setOnClickListener(view -> {
+            boolean temp = View.VISIBLE != extrasInputCtn.getVisibility();
+
+            extraPriceInput.setText("");
+            nameInput.setText("");
+
+            if(temp){
+                extrasInputCtn.setVisibility(View.VISIBLE);
+                nameInput.requestFocus();
+                imm.showSoftInput(nameInput, InputMethodManager.SHOW_IMPLICIT);
+            }else{
+                extrasInputCtn.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        nameInput.setOnEditorActionListener((textView, actionId, keyEvent) -> {
+            if(actionId == EditorInfo.IME_ACTION_DONE){
+                extraPriceInput.requestFocus();
+                imm.showSoftInput(extraPriceInput, InputMethodManager.SHOW_IMPLICIT);
+                return true;
+            }
+            return false;
+        });
+
+        extraPriceInput.setOnEditorActionListener((textView, actionId, keyEvent) -> {
+            if(actionId == EditorInfo.IME_ACTION_DONE){
+                String check = extraPriceInput.getText().toString();
+
+                if(!"".equals(check) && !".".equals(check) && !"-".equals(check)){
+                    extrasPrices.add((double)Math.round(100 * Double.parseDouble(check))/100.0);
+                    extras.add(nameInput.getText().toString());
+
+                    extrasInputCtn.setVisibility(View.INVISIBLE);
+                    updateExtrasList();
+                    imm.hideSoftInputFromWindow(textView.getWindowToken(), 0);
+                }else{
+                    Toast.makeText(MainActivity.this, "Invalid Price Entered!", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+            return false;
+        });
+
+        calculate.setOnClickListener(view -> calculateTotal());
+
         //notes:
         /*
         on image view pressed change between toggled on text and toggled off text, make custom image for it
@@ -219,11 +298,18 @@ public class MainActivity extends AppCompatActivity {
 
 
         replace full body/half body/headshot with custom text
+
+
+        if button pressed:
+        open edit screen, set as item in list that is being edited, have special section on hitting enter
+
+        if edit visible and current button is set as being visible, delete the entry
         */
     }
 
     public void updatePriceText(){
         preExtraFinal = 0;
+        totalDiscount = 0;
 
         String out = "";
 
@@ -300,6 +386,7 @@ public class MainActivity extends AppCompatActivity {
 
             preExtraFinal += costTot;
             preExtraFinal -= dscTot;
+            totalDiscount += dscTot;
             out += "\n================================\n";
             out +="Cost:                $ " + String.format(Locale.ROOT,"%,.2f", costTot) + "\n";
             out +="                                      -$ " + String.format(Locale.ROOT,"%,.2f", dscTot) + "\n";
@@ -341,6 +428,7 @@ public class MainActivity extends AppCompatActivity {
 
             preExtraFinal += costTot;
             preExtraFinal -= dscTot;
+            totalDiscount += dscTot;
             out += "\n================================\n";
             out +="Cost:                $ " + String.format(Locale.ROOT,"%,.2f", costTot) + "\n";
             out +="                                      -$ " + String.format(Locale.ROOT,"%,.2f", dscTot) + "\n";
@@ -382,20 +470,54 @@ public class MainActivity extends AppCompatActivity {
 
             preExtraFinal += costTot;
             preExtraFinal -= dscTot;
+            totalDiscount += dscTot;
             out += "\n================================\n";
             out +="Cost:                $ " + String.format(Locale.ROOT,"%,.2f", costTot) + "\n";
             out +="                                      -$ " + String.format(Locale.ROOT,"%,.2f", dscTot) + "\n";
             out +="SubTotal:        $ " + String.format(Locale.ROOT,"%,.2f", preExtraFinal) + "\n\n";
         }
-        if(fb+hb+hs != 0){out += "================================\n";}
-
+        //if(fb+hb+hs != 0){out += "================================\n";}
 
         pricesFieldText.setText(out);
     }
 
     public void copyText(){
+        //TODO: Remake this with all format somehow
+
+        double[] finals = calculateTotal();
+
+        //out += "================================\n";
+        String temp = "";
+
+
+        if(fb+hb+hs > 0){
+            temp += pricesFieldText.getText().toString();
+        }
+
+        if(extras.size() > 0){
+            temp += "EXTRA CHARGES===================\n";
+            temp += "================================\n";
+
+            for(int i = 0; i < extras.size(); i++){
+                double curprice = extrasPrices.get(i);
+                String out;
+
+                if(curprice < 0){out = extras.get(i) + ":   -$ " + String.format(Locale.ROOT, "%,6.2f" ,curprice) + "\n";}
+                else{out = extras.get(i) + ":   $ " + String.format(Locale.ROOT, "%,6.2f" ,curprice) + "\n";}
+                temp += String.format(Locale.ROOT, "%33s", out);
+            }
+
+            temp += "\n";
+        }
+
+        temp += "FINAL PRICE=======================\n";
+        temp += "================================\n";
+        temp += String.format(Locale.ROOT, "SUB:                 $ %,.2f" ,finals[0] + finals[1]) + "\n";
+        temp += String.format(Locale.ROOT, "                                      -$ %,.2f" ,finals[0]) + "dsc\n";
+        temp += String.format(Locale.ROOT, "FIN:                  $ %,.2f" ,finals[1]) + "\n";
+
+
         ClipboardManager cb = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-        String temp = pricesFieldText.getText().toString();
         ClipData out = ClipData.newPlainText("output", temp);
         cb.setPrimaryClip(out);
 
@@ -418,5 +540,92 @@ public class MainActivity extends AppCompatActivity {
 
         updatePriceText();
         shdCtn.setVisibility(View.INVISIBLE);
+    }
+
+    public void updateExtrasList(){
+        extrasList.removeViewsInLayout(0, extrasList.getChildCount());
+
+        /*
+            if(deleteMode){
+                buttonColor = new ContextThemeWrapper(this, R.style.BJG_RedButton);
+            }else if(renameMode){
+                buttonColor = new ContextThemeWrapper(this, R.style.BJG_GreenButton);
+            }else{
+                buttonColor = new ContextThemeWrapper(this, R.style.Base_Widget_AppCompat_Button_Colored);
+            }
+
+             Button test = new Button(buttonColor);
+
+
+            button pressed: open edit for that view, set as one being edited. if delete button pressed, change button color to red, if button red, delete.
+
+            beingedited - integer that stores the current id of the button in the list that was pressed.
+
+            on edit: initialize ready to delete as false, create button programatically.
+            if pressed && !readytodelete->readytodlete=true
+            if pressed && readytodelete->delete beingedited
+         */
+
+        for(int i = 0; i < extras.size(); i++){
+            Resources r = getResources();
+
+            Button temp = new Button(new ContextThemeWrapper(this, R.style.MainActivity_priceButton));
+
+            double curprice = extrasPrices.get(i);
+            String text = "";
+
+            if(curprice < 0){text = extras.get(i) + ":   -$ " + String.format(Locale.ROOT, "%,6.2f" ,Math.abs(curprice)) + "           ";}
+            else{text = extras.get(i) + ":   $ " + String.format(Locale.ROOT, "%,6.2f" ,curprice) + "           ";}
+
+
+            temp.setText(text);
+            temp.setTransformationMethod(null);
+            temp.setGravity(Gravity.CENTER_VERTICAL | Gravity.END);
+            temp.setBackgroundColor(Color.parseColor("#FF4F4561"));
+
+            float height = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,20f,r.getDisplayMetrics());
+            temp.setHeight((int)height);
+            temp.setMinimumHeight((int)height);
+            temp.setPadding(0,0,0,0);
+
+
+            float margin = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,5f,r.getDisplayMetrics());
+            LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            param.setMargins(0,(int)margin,0,0);
+
+            temp.setLayoutParams(param);
+
+
+            extrasList.addView(temp);
+        }
+    }
+
+    public double[] calculateTotal(){
+        double finalPrice = preExtraFinal;
+        double finalDiscTot = totalDiscount;
+
+        for(double x : extrasPrices){
+            finalPrice += x;
+            if(x<0){
+                finalDiscTot -= x;
+            }
+        }
+
+        finalDiscText.setText("Dsc: -$ " + String.format(Locale.ROOT, "%,.2f" ,finalDiscTot));
+        finalPriceText.setText("Tot: $ " + String.format(Locale.ROOT, "%,.2f" ,finalPrice));
+
+        return new double[]{finalDiscTot, finalPrice};
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if(shdCtn.getVisibility() == View.VISIBLE){
+            shdCtn.setVisibility(View.INVISIBLE);
+        }else if(extrasInputCtn.getVisibility() == View.VISIBLE){
+            extrasInputCtn.setVisibility(View.INVISIBLE);
+        }else {
+            super.onBackPressed();
+        }
     }
 }
